0d0691ee5a2bd2ed5e0fd14277ff90babfa90ab6
Ivan Sosnin <sosnin.spb@yandex.ru>
2025-04-27 02:32:39 +0300

    FINAL_2: fix UpdateCompilationRequest

--- ewm-main/src/main/java/ru/practicum/dto/compilation/UpdateCompilationRequest.java
+++ ewm-main/src/main/java/ru/practicum/dto/compilation/UpdateCompilationRequest.java
@@ -1,6 +1,5 @@
 package ru.practicum.dto.compilation;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,7 +9,6 @@ import java.util.List;
 public class UpdateCompilationRequest {
 	private Boolean pinned;
 
-	@NotBlank
 	@Size(min = 1, max = 50)
 	private String title;
 
